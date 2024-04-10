d0b06b4b87b67b1fee1e9cedb90209a846809465
dongwoohhh <dongwoohhh@gmail.com>
2024-04-10 14:43:05 +0900

    option for kernel layer

--- basicsr/models/archs/KernelNet_arch.py
+++ basicsr/models/archs/KernelNet_arch.py
@@ -719,13 +719,14 @@ class BlurCLIP(nn.Module):
     def __init__(self,
                  kernel_size=61,
                  vision_layers=[3,4,6,3],
+                 kernel_layers=2,
                  #vision_layers: Union[Tuple[int, int, int, int], int],
                  #vision_width: int,
                  #vision_patch_size: int,
                  ):
         super().__init__()
 
-        self.k_encoder = KernelMLPMixerEncoder(kernel_size=kernel_size, output_dim=128, token_dim=128, channel_dim=128, depth=2)
+        self.k_encoder = KernelMLPMixerEncoder(kernel_size=kernel_size, output_dim=128, token_dim=128, channel_dim=128, depth=kernel_layers)
         #self.k_encoder = KernelAttentionEncoder(inner_dim=32, output_dim=128, depth=2, heads=4)
         self.b_encoder = BlurEncoder(layers=vision_layers, output_dim=128, width=64)
         
